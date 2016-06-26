9ca8d782e69ca0282db0f7549ab0ee6dd958ea6f
Rafael Fazzolino <fazzolino29@gmail.com>
2016-06-26 19:27:42 -0300

    Gerando vetor de pontos a cada 45 graus

--- src/controle/Main.java
+++ src/controle/Main.java
@@ -1,70 +1,63 @@
 package controle;
 
-
-import java.util.HashMap;
-import java.util.Map;
-
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.navigation.DifferentialPilot;
 
 public class Main {
-
+	private static double [] pontosX = new double[15];
+	private static double [] pontosY = new double[15];
 	
 	public static void main(String[] args) throws Exception {
 		UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S1);
 		Main main = new Main();
 		
-		int [] distancia = new int[362];
-		Map<Integer, Integer> distancias = new HashMap<Integer,Integer>();
-		Map<Integer, String> pontos = new HashMap<Integer, String>();
-		
 		int cont=0;
-		int angle = 0;
-				while(cont < 360) {
+		double angle = 0;
+		System.out.println("COMEÇOU");
+				while(angle < 360) {
 					if(sonic.getDistance() != 255){
-						System.out.println("Pegando distancia..");
-						distancia[cont] = sonic.getDistance();
-						System.out.println(distancia[cont]);
-						distancias.put(angle,distancia[cont]);//}Salvando a distancia no mapa
+						System.out.println("Pegando distancia.." + sonic.getDistance());
+						geraPontos(sonic.getDistance(), angle, cont);
 						System.out.println("Salvou ponto..");
 						Thread.sleep(2000);
-						main.rotate(90);
+						main.rotate(45);
+						angle +=45;
 						System.out.println("Girou..");
-						angle += 90;
-						cont+=90;
+						System.out.println("Angulo atual:" +angle + "\ncont atual:"+cont);
+						cont++;
 					}
 				}
-				System.out.println("Definindo pontos..");
-				pontos = geraPontos(distancias);
-				Thread.sleep(2000);
-				System.out.println("Resultado dos pontos....:\n");
-				System.out.println(pontos.toString());
+				
+				System.out.println("O resultado dos pontos e: " );
+				for(int i=0; i<pontosX.length ; i++){
+					System.out.println("(X:"+pontosX[i]+", Y="+pontosY[i]+")");
+				}
 				
 	}
 	
-	private static Map<Integer, String> geraPontos(Map<Integer, Integer> distancias) {
-		int cont=0;
-		Map<Integer, String> pontos = new HashMap<Integer, String>();
-		while(cont < 360){
-			if(cont == 0 || cont == 180){
-				pontos.put(cont, 0+","+distancias.get(cont));
-			} else{
-				pontos.put(cont, distancias.get(cont)+","+0);
-			}
-			cont +=90;
-			
-		}
-		
-		return pontos;
-		
+	private static void geraPontos(int distancia, double angle, int cont) throws InterruptedException {
+		pontosX[cont] = round(Math.sin(Math.PI/180*angle)*distancia, 0);
+		pontosY[cont] = round(Math.cos(Math.PI/180*angle)*distancia, 0);
+		System.out.println("Ponto criado: x="+pontosX[cont] +";y="+pontosY[cont]);
+		Thread.sleep(600);
+	}
+	
+	public static double round(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+
+	    long factor = (long) Math.pow(10, places);
+	    value = value * factor;
+	    long tmp = Math.round(value);
+	    return (double) tmp / factor;
 	}
 
-	public void rotate(double angle){
+	public double rotate(double angle){
 		DifferentialPilot pilot = new DifferentialPilot(36,126,Motor.B, Motor.A,true);
 		pilot.setRotateSpeed(50);
-		pilot.rotate(angle*1.6);	//Valor para ajustar erro de rotação.
+		pilot.rotate(angle*1.7);	//Valor para ajustar erro de rotação.
+		return angle;
 	}
 
 }
